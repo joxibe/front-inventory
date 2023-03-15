3f083bd65dc718b0cad25fd7c7420f0ed9e24664
JOSE_ROSERO\jose2 <jxrosero1gmail.com>
2023-03-15 14:47:02 -0500

    sidenav

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './pages/dashboard.component';
 import { HomeComponent } from './compotents/home/home.component';
 import { RouterModule } from '@angular/router';
+import { SharedModule } from '../shared/shared.module';
 
 
 
@@ -13,7 +14,9 @@ import { RouterModule } from '@angular/router';
   ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    SharedModule
+
   ]
 })
 export class DashboardModule { }
